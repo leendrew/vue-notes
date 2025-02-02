e096028741968007bc92b84600b2671360416fa5
leendrew <leendrew99@gmail.com>
2025-02-02 20:43:18 +0300

    feat: styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import { router } from '@/router';
 import './main.css';
 import App from './App.vue';
 
-createApp(App).use(router).mount('#app');
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount('#app');
