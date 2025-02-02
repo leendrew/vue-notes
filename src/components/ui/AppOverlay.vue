9de2cbb61256be0c22250beb6b35a37721b79f68
leendrew <leendrew99@gmail.com>
2025-02-02 23:38:16 +0300

    feat: add overlay ui component

---
+++ src/components/ui/AppOverlay.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="overlay">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  position: absolute;
+  inset: 0;
+  background-color: var(--color-dark);
+  opacity: 0.7;
+}
+</style>
