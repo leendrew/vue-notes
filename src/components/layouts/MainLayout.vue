4890b369680b2b8b75d9c20fd8c74231cd57f769
leendrew <leendrew99@gmail.com>
2025-02-02 15:58:38 +0300

    feat: main layout

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue';
+</script>
+
+<template>
+  <AppHeader />
+  <main class="main">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.main {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+}
+</style>
