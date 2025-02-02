4c08a659087ea2c87c2c9f74a5a635e515d22664
leendrew <leendrew99@gmail.com>
2025-02-02 15:56:05 +0300

    feat: button ui component

---
+++ src/components/ui/AppButton.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+interface AppButtonProps {
+  icon?: boolean;
+}
+
+const { icon = false } = defineProps<AppButtonProps>();
+</script>
+
+<template>
+  <button
+    class="button"
+    :class="{
+      icon,
+    }"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  --btn-size: 56px;
+  --btn-bg-color: var(--color-green-light);
+  --btn-padding-inline: 24px;
+
+  height: var(--btn-size);
+  padding-inline: 24px;
+  background-color: var(--btn-bg-color);
+  color: var(--color-white);
+  border: 0px solid transparent;
+  border-radius: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+}
+
+.button:hover {
+  --btn-bg-color: var(--color-green-middle);
+}
+
+.button:active {
+  --btn-bg-color: var(--color-green-dark);
+}
+
+.button:disabled {
+  --btn-bg-color: var(--color-gray);
+}
+
+.button.icon {
+  --btn-padding-inline: 0;
+
+  width: var(--btn-size);
+}
+</style>
