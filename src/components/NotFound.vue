d25759b5102d36ab2609428db629c90960b7481d
leendrew <leendrew99@gmail.com>
2025-02-02 21:17:47 +0300

    feat: add 404 page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AppContainer from '@/components/ui/AppContainer.vue';
+import AppTypography from '@/components/ui/AppTypography.vue';
+import AppLink from '@/components/ui/AppLink.vue';
+import { routeConfig } from '@/config';
+</script>
+
+<template>
+  <section class="error">
+    <AppContainer class="error_inner">
+      <AppTypography
+        is="h1"
+        variant="h2"
+      >
+        Ошибка 404. Страница не найдена
+      </AppTypography>
+      <AppLink :to="{ name: routeConfig.home.name }">
+        <AppTypography> Вернуться на главную </AppTypography>
+      </AppLink>
+    </AppContainer>
+  </section>
+</template>
+
+<style scoped>
+.error_inner {
+  padding-block: 160px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
