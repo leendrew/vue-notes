248c4c5987d7b9e3f23fbb88ab80a3d1849a17da
leendrew <leendrew99@gmail.com>
2025-02-02 21:17:39 +0300

    feat: add link ui component

---
+++ src/components/ui/AppLink.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { RouterLinkProps } from 'vue-router';
+
+interface AppLinkProps extends RouterLinkProps {}
+
+const { to, ...rest } = defineProps<AppLinkProps>();
+</script>
+
+<template>
+  <RouterLink
+    class="link"
+    :to="to"
+    v-bind="{ ...rest, ...$attrs }"
+  >
+    <slot />
+  </RouterLink>
+</template>
+
+<style scoped>
+.link {
+  --link-color: var(--color-green-light);
+
+  color: var(--link-color);
+}
+
+.link:hover {
+  --link-color: var(--color--white);
+}
+</style>
