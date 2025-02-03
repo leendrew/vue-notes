a42f559a668462b9b849c543cc906db3d3d29b8f
leendrew <leendrew99@gmail.com>
2025-02-03 11:22:53 +0300

    feat: add login button

---
+++ src/components/auth/LoginButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppButton from '@/components/ui/AppButton.vue';
+import AppTypography from '@/components/ui/AppTypography.vue';
+import LoginIcon from '@/components/icons/LoginIcon.vue';
+import AuthModal from './AuthModal.vue';
+
+const isAuthModalShow = ref<boolean>(false);
+
+const onLoginClick = () => {
+  isAuthModalShow.value = true;
+};
+</script>
+
+<template>
+  <AppButton @click="onLoginClick">
+    <LoginIcon />
+    <AppTypography>Вход</AppTypography>
+  </AppButton>
+  <template v-if="isAuthModalShow">
+    <AuthModal v-model="isAuthModalShow" />
+  </template>
+</template>
