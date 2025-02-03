45fcb4575b3eaa2f1b76c3e582635170dcbcfed8
leendrew <leendrew99@gmail.com>
2025-02-03 12:09:40 +0300

    chore: cleanup

---
+++ src/components/notes/AddNoteButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import AppButton from '@/components/ui/AppButton.vue';
+import AddNoteModal from './AddNoteModal.vue';
+import PlusIcon from '@/components/icons/PlusIcon.vue';
+
+const isAddNoteModalShow = ref<boolean>(false);
+
+const onAddNoteClick = () => {
+  isAddNoteModalShow.value = true;
+};
+</script>
+
+<template>
+  <AppButton
+    class="button"
+    icon
+    @click="onAddNoteClick"
+  >
+    <PlusIcon />
+    <template v-if="isAddNoteModalShow">
+      <AddNoteModal v-model="isAddNoteModalShow" />
+    </template>
+  </AppButton>
+</template>
+
+<style scoped>
+.button {
+  box-shadow: 0px 15px 46px -10px rgba(0, 0, 0, 0.6);
+}
+</style>
