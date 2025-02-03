4474e8a596a6a77730a56b82f17de874cccb296c
leendrew <leendrew99@gmail.com>
2025-02-03 11:21:36 +0300

    feat: core logic

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from './store';
+import { routeConfig } from './config';
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const bootstrap = async () => {
+  await authStore.getMyself();
+  if (!authStore.isAuth) {
+    return;
+  }
+
+  router.push({ name: routeConfig.notes.name });
+};
+
+onMounted(() => {
+  bootstrap();
+});
+</script>
+
 <template>
   <RouterView />
 </template>
