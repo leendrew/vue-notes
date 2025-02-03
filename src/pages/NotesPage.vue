ca107ebdaf1730d31aba0dae62bad948fd6ba79d
leendrew <leendrew99@gmail.com>
2025-02-03 12:08:28 +0300

    feat: add notes page

---
+++ src/pages/NotesPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MainLayout from '@/components/layouts/MainLayout.vue';
+import NotesList from '@/components/notes/NotesList.vue';
+</script>
+
+<template>
+  <MainLayout>
+    <NotesList />
+  </MainLayout>
+</template>
