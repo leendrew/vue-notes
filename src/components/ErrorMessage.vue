c66a4abc8849a027c23157721f8b7a0e7ac59409
leendrew <leendrew99@gmail.com>
2025-02-03 11:23:59 +0300

    feat: add error message component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import AppTypography from '@/components/ui/AppTypography.vue';
+
+interface ErrorMessageProps {
+  text: string;
+}
+
+const { text } = defineProps<ErrorMessageProps>();
+</script>
+
+<template>
+  <div class="error">
+    <AppTypography
+      class="error__text"
+      variant="small"
+    >
+      {{ text }}
+    </AppTypography>
+  </div>
+</template>
+
+<style scoped>
+.error {
+  padding-block: 8px;
+  padding-inline: 20px;
+  border-radius: 24px;
+  background-color: rgba(255, 116, 97, 0.1);
+}
+
+.error__text {
+  color: var(--color-red);
+}
+</style>
